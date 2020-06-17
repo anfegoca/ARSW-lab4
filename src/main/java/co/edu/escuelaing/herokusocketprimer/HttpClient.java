6aab9f29327549c528bb935f37d41d0c4619c833
Andres Gonzalez <andres.gonzalez-ca@mail.escuelaing.edu.co>
2020-06-16 21:51:56 -0500

    Documentación

--- src/main/java/co/edu/escuelaing/herokusocketprimer/HttpClient.java
+++ src/main/java/co/edu/escuelaing/herokusocketprimer/HttpClient.java
@@ -6,32 +6,11 @@ import java.net.*;
 public class HttpClient {
 
     public static void main(String[] args) throws IOException {
-
-        Socket echoSocket = null;
-        PrintWriter out = null;
-        BufferedReader in = null;
-
-        try {
-            echoSocket = new Socket("127.0.0.1", 35000);
-            out = new PrintWriter(echoSocket.getOutputStream(), true);
-            in = new BufferedReader(new InputStreamReader(
-                    echoSocket.getInputStream()));
-        } catch (UnknownHostException e) {
-            System.err.println("Don’t know about host!.");
-            System.exit(1);
-        } catch (IOException e) {
-            System.err.println("Couldn’t get I/O for "
-                    + "the connection to: localhost.");
-            System.exit(1);
-        }
-        
-        
-        out.println(args[0]);
-        
-        
-        out.close();
+        URL pagina = new URL("http://localhost:35000/"+args[0]);
+        URLConnection connection = pagina.openConnection();
+        BufferedReader in = new BufferedReader(new InputStreamReader(
+                                    connection.getInputStream()));
         in.close();
-        
-        echoSocket.close();
+
     }
 }
--- src/test/java/co/edu/escuelaing/herokusocketprimer/HttpServerTest.java
+++ src/test/java/co/edu/escuelaing/herokusocketprimer/HttpServerTest.java
@@ -15,41 +15,36 @@ import org.junit.Test;
 
 /**
  *
- * @author Imac
+ * @author Andres Gonzalez
  */
 public class HttpServerTest {
     @Test
     public void Client(){
+        String[] args = {"index.html"};
         ExecutorService pool = Executors.newFixedThreadPool(102);
-        pool.submit(new Runnable(){
-            public void run(){
-                try {
-                    HttpServer.main(null);
-                } catch (IOException ex) {
-                    Logger.getLogger(HttpServerTest.class.getName()).log(Level.SEVERE, null, ex);
-                }
+        pool.submit(() -> {
+            try {
+                HttpServer.main(args);
+            } catch (IOException ex) {
+                Logger.getLogger(HttpServerTest.class.getName()).log(Level.SEVERE, null, ex);
             }
         });
-        String[] args = {"GET /pagina/index.html"};
+        
         for(int i=0;i<100;i++){
-            pool.submit(new Runnable(){
-                public void run(){
-                    try {
-                        HttpClient.main(args);
-                    } catch (IOException ex) {
-                        Logger.getLogger(HttpServerTest.class.getName()).log(Level.SEVERE, null, ex);
-                    }
-                }
-            });
-        }
-        args[0] = "bye";
-        pool.submit(new Runnable(){
-            public void run(){
+            pool.submit(() -> {
                 try {
                     HttpClient.main(args);
                 } catch (IOException ex) {
                     Logger.getLogger(HttpServerTest.class.getName()).log(Level.SEVERE, null, ex);
                 }
+            });
+        }
+        args[0] = "bye";
+        pool.submit(() -> {
+            try {
+                HttpClient.main(args);
+            } catch (IOException ex) {
+                Logger.getLogger(HttpServerTest.class.getName()).log(Level.SEVERE, null, ex);
             }
         });
         pool.shutdown();
